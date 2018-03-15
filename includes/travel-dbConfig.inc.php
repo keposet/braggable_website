225ce59b4c18a8c8715e2c65521405277f35b422
Jason Hutson <jhuts645@mtroyal.ca>
2018-03-15 21:09:01 +0000

    travel DB config file

---
+++ includes/travel-dbConfig.inc.php
@@ -0,0 +1,27 @@
+<?php
+// set error reporting on for debugging
+error_reporting(E_ALL);
+ini_set('display_errors','1');
+
+/* note: connection for cloud9 */
+$ip = getenv('IP');
+$port = '3306';
+$dbname ='travel';
+
+// db connection string
+define('DBCONNECTION', "mysql:host=$ip;port=$port;dbname=$dbname;charset=utf8mb4;");
+define('DBUSER', getenv('kepposet'));
+define('DBPASS', '');
+
+// auto load all classes to reduce includes
+spl_autoload_register(function ($class) {
+ $file = 'lib/' .$class. '.class.php';
+ if (file_exists($file))
+ include $file;
+});
+
+// connect to the database
+// Once included this variable can be passed to data gateways to query the DB
+$connection = DatabaseHelper::createConnectionInfo(array(DBCONNECTION, DBUSER, DBPASS));
+
+?>
