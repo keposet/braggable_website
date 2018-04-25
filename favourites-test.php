d371ffaa7e1b1629e3d28314a8225193342aa486
Jason Hutson <jhuts645@mtroyal.ca>
2018-04-25 09:34:05 +0000

    cookie skeleton complete.

--- favourites-test.php
+++ favourites-test.php
@@ -1,15 +1,49 @@
 <?php
 require 'includes/travel-dbConfig.inc.php'; 
 
+//print_r($read); 
+$postCookie = $_COOKIE['post'];
+$imgCookie = $_COOKIE['image']; 
+
+
+if (isset($postCookie) && !empty($postCookie)) {
+    // load values into an array, feed that array into the database
+    $postData = cookieDataToArray($postCookie);
+    // print_r($postData);
+}
+
+if(isset($imgCookie) && !empty($imgCookie)){
+    $imgData = cookieDataToArray($imgCookie);
+}
+
+
+function cookieDataToArray($data){
+    return explode(" ", $data);
+}
+
+
+
 
 try {
     $result = new PostsGateway($connection);
     // what do i need? really just a title and an image cause why the hell not
     $posts = $result -> findByStatement(2,null);
     
+    $postList = array(); 
+    foreach($postData as $pD){
+        array_push($postList,$result -> findById($pD));
+    }
+    
+    print_r($postList);
+    
     $result = new ImageDetailsGateway($connection); 
     $images = $result -> findByStatement(0, null);
     // has the big arrays with everything
+    
+    $imgList = array();
+    foreach($imgData as $iD){
+        array_push($imgList, $result-> findById($iD)); 
+    }
     
     
 } catch (Exception $e ) {
@@ -25,8 +59,15 @@ try {
 <html>
     <body>
         <div class = "view_fave">
-            <button id =test> Click</button>
+            
         </div>
+        
+        <div class = "hidden" id = "alertParent">
+            <div class = "hidden alert" id = "addMessage">
+                Added To Favourites
+            </div>    
+        </div>
+        
 
         <div class ="content" style="display:flex; margin: 50px">
             <div class ="pics_content" style="margin:1em">
@@ -69,92 +110,134 @@ try {
     </body>
     
     <script>
-        function cookiesToArray(){
-            let cookies = document.cookie; 
-                cookies = decodeURIComponent(cookies); 
-                // cookies is now an array of strings
-            return cookies = cookies.split("; ");
+   
+    
+        function wholeCookiesToArray(){
+            let cookiesArr = document.cookie; 
+                cookiesArr = decodeURIComponent(cookiesArr); 
+                
+                
+            return cookiesArr = cookiesArr.split("; ");
         }
         
-        function cookieValuesToArray(cookies, key){
-            // go in on finding the value
-            let values = cookies;
+        
+        // returns an array from a cookie c=12345 -> [1 2 3 4 5]
+        function cookieValuesToArray(cookie, key){
+            
+            let values = cookie;
             // cookies at i is a string, need to substring, split iterate
             values = values.substring(key.length+1);
-            return values = values.split(",");
+            
+            return values = values.split(" ");
+            // return values = values.split(" ");
             
         }
             
-            
-        let tb = document.getElementById("test");
-        tb.addEventListener("click",function(){
-            console.log();
-            
-            
-        });
-
-        
+        window.load = function(){
         let content = document.querySelector(".content");
-        content.addEventListener("click", function(e){
-            
-            let values;
-            console.log(e.target.getAttribute("class"));
+        
+        content.addEventListener("click", function(e){    
+        
+            // ADD to Fave
             if(e.target && (e.target.getAttribute("class") == "img_fav" || e.target.getAttribute("class") == "post_fav")){
-                console.log(e.target.getAttribute("class"))
-                // all this will be needed for removal
+               let values;
+                //console.log(e.target.getAttribute("class"))
+                
+
                 let key = e.target.getAttribute("name");
                 let val = e.target.getAttribute("value");
+                console.log("val at instantiation " + val);
+               
+               // creates a new cookie with only one value
                 let newcookie = true
+                // adds to the list of cookies 
                 let appendCookie = true;
                 
-                let cookies = cookiesToArray();
+                let cookies = wholeCookiesToArray();
                 
                 for(let i = 0; i<cookies.length; i++){
                     // check for existing cookies
                     if(cookies[i].startsWith(key)){
                         // key exists
                         newcookie = false;
+                        console.log(" found key" +cookies[i]);
                         values = cookieValuesToArray(cookies[i], key);
+                        //console.log("values after array func " + values);
                         
+                        // check for existence of value in string
                         for(let i = 0; i < values.length; i++){
+                            console.log(val);
                             if(values[i] == val){
+                                //console.log("value at i "+ values[i]);
+                                
+                                //why 
+                                        // key exists and value exists 
                                 appendCookie = false;
                             }
                         }
                         if(appendCookie){
-                            values+= ","+val; 
-                        }
-                        values = values.toString();
-                    }
-                }
-                // either adds a new cookie, or overwrites with added value
-                newcookie ? document.cookie = key+"="+val : document.cookie = key +"=" +values
-                // remove either do nothing, or overwrite w/o target 
-            // }else if(e.target && (e.target.getAttribute("class") == "post_rem" || e.target.getAttribute("class") == "img_rem")){
-            }
-             if(e.target && (e.target.getAttribute("class") == "post_rem" || e.target.getAttribute("class") == "img_rem")){
-                let key = e.target.getAttribute("name");
-                let val = e.target.getAttribute("value");
-                let cookies = cookiesToArray();
-                
-                for(let i = 0; i < cookies.length;i++){
-                    if(cookies[i].startsWith(key)){
-                        
-                        values = cookieValuesToArray(cookies[i],key);
-                        for(let i =0; i< values.length; i++){
-                            if(values[i] == val){
-                                //values = values.splice(i,1);
-                                values.splice(i,1);
+                            console.log("values length " + values.length); 
+                            console.log("values [i] " + values[i]); 
+                            if(values.length >=1 ){
+                                //values+= " "+val;
+                                values.push(val);
+                                console.log(values);
+                              
                             }
+                            
+                          //  values+= " "+val;
+                            console.log(values);
+                           // values = val; 
                         }
-                        
+                        //console.log(values);
+                        // seems like this isn't needed 
+                        values = values.join(" ");
+                        console.log(values);
+                        console.log(typeof values);
                     }
                 }
-                console.log(values);
-                document.cookie = key +"="+values;
+ 
+               // newcookie ? document.cookie = key +"="+ JSON.stringify(values) : document.cookie = key +"="+ JSON.stringify(values);
+               
+                newcookie ?  document.cookie = key +"="+ val : document.cookie = key +"="+ values;
+                
+                // display div on add
+                let messageDiv = document.getElementById("addMessage");
+                messageDiv.setAttribute("class", "alert");
+                setTimeout(function(){messageDiv.setAttribute("class", "hidden alert")}, 2000);
+                
+                
             }
+            // REMOVE from Fave
+            
+            // this is a hard remove. need to be able to queue for removal. 
+                 if(e.target && (e.target.getAttribute("class") == "post_rem" || e.target.getAttribute("class") == "img_rem")){
+                    let values; 
+                    let key = e.target.getAttribute("name");
+                    let val = e.target.getAttribute("value");
+                    let cookies = wholeCookiesToArray();
+                    
+                    for(let i = 0; i < cookies.length;i++){
+                        if(cookies[i].startsWith(key)){
+                            
+                            values = cookieValuesToArray(cookies[i],key);
+                            for(let i =0; i< values.length; i++){
+                                if(values[i] == val){
+                                    //values = values.splice(i,1);
+                                    values.splice(i,1);
+                                }
+                            }
+                            
+                        }
+                    }
+                    console.log(values);
+                    values = values.join(" ");
+                    document.cookie = key +"="+values;
+                }
+            
             console.log(document.cookie);
         }); 
-        
+    
+        }();
     </script>
 </html>
--- resources-used.php
+++ resources-used.php
@@ -1,3 +1,4 @@
 Extract Column Names from Database Table: https://stackoverflow.com/questions/5525970/mysql-show-columns-but-exclude-everything-except-the-field-names
 Event Delegation in Javascript tutorial : https://davidwalsh.name/event-delegate
 Ternary operators in javascript : https://www.digitalocean.com/community/tutorials/how-to-write-conditional-statements-in-javascript
+javascript cookies built off: https://www.w3schools.com/js/js_cookies.asp
